933d074ad6704f46f63e6df14e8962a61f82a9c7
GiuranRadu <117910448+GiuranRadu@users.noreply.github.com>
2024-04-24 13:34:40 +0300

    server little change

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const dotenv = require('dotenv'); //!astea trebuie sa fie inaintea constantei `a
 dotenv.config({ path: './config.env' }); //!astea trebuie sa fie inaintea constantei `app`
 
 //-> SAFETY NET error handlers -> must be on the top level of the page, before `app`
-process.on('uncaughtException' , (err) => {
+process.on('uncaughtException', (err) => {
+  console.log(err.stack);
   // console.log(err.name, err.message);
-  // console.log('❌ Uncaught exception occured! Shutting down... ❌');
-  // server.close(() => {
-  //   process.exit(1)
-  // });
+  console.log('❌ Uncaught exception occured! Shutting down... ❌');
 });
 
 const app = require('./app');
@@ -22,5 +20,5 @@ mongoose.connect(process.env.CONN_STR, {
 })
 
 const server = app.listen(port, () => {
-  console.log(`Server has started on port: ${port}... ✅ ` );
+  console.log(`Server has started on port: ${port}... ✅ `);
 });
