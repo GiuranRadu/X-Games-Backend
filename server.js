9d1f89a36ff81af78f6302131c076893c756c054
GiuranRadu <117910448+GiuranRadu@users.noreply.github.com>
2024-05-08 12:34:15 +0300

    changed port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ process.on('uncaughtException', (err) => {
 });
 
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 
 
